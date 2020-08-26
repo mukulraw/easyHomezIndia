07adbc771c13598c0459d2cf8fd582e55e208694
mukul <mukulraw199517@gmail.com>
2020-08-26 16:27:54 +0530

    easyHomezIndia

--- app/src/main/java/com/technuoma/easyHomezIndia/SingleProduct.java
+++ app/src/main/java/com/technuoma/easyHomezIndia/SingleProduct.java
@@ -6,7 +6,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,16 +16,26 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.technuoma.easyHomezIndia.homePOJO.Banners;
 import com.technuoma.easyHomezIndia.seingleProductPOJO.Data;
 import com.technuoma.easyHomezIndia.seingleProductPOJO.singleProductBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import me.relex.circleindicator.CircleIndicator;
 import nl.dionsegijn.steppertouch.StepperTouch;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -37,16 +49,18 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public class SingleProduct extends AppCompatActivity {
 
     Toolbar toolbar;
-    ImageView image;
-    TextView discount , title , price;
+    ViewPager image;
+    TextView discount, title, price;
     Button add;
-    TextView brand , unit , seller;
-    TextView description , key_features , packaging , life , disclaimer , stock;
+    TextView brand, unit, seller;
+    TextView description, key_features, packaging, life, disclaimer, stock;
     ProgressBar progress;
 
-    String id , name;
+    String id, name;
 
-    String pid , nv1;
+    String pid, nv1;
+
+    CircleIndicator indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +71,7 @@ public class SingleProduct extends AppCompatActivity {
         name = getIntent().getStringExtra("title");
 
         toolbar = findViewById(R.id.toolbar);
+        indicator = findViewById(R.id.indicator);
         image = findViewById(R.id.image);
         discount = findViewById(R.id.discount);
         title = findViewById(R.id.title);
@@ -92,12 +107,10 @@ public class SingleProduct extends AppCompatActivity {
             public void onClick(View view) {
 
 
-                if (pid.length() > 0)
-                {
+                if (pid.length() > 0) {
                     String uid = SharePreferenceUtils.getInstance().getString("userId");
 
-                    if (uid.length() > 0)
-                    {
+                    if (uid.length() > 0) {
 
                         final Dialog dialog = new Dialog(SingleProduct.this);
                         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -105,10 +118,9 @@ public class SingleProduct extends AppCompatActivity {
                         dialog.setContentView(R.layout.add_cart_dialog);
                         dialog.show();
 
-                        final StepperTouch stepperTouch  = dialog.findViewById(R.id.stepperTouch);
+                        final StepperTouch stepperTouch = dialog.findViewById(R.id.stepperTouch);
                         Button add = dialog.findViewById(R.id.button8);
                         final ProgressBar progressBar = dialog.findViewById(R.id.progressBar2);
-
 
 
                         stepperTouch.setMinValue(1);
@@ -139,22 +151,21 @@ public class SingleProduct extends AppCompatActivity {
                                         .build();
                                 AllApiIneterface cr = retrofit.create(AllApiIneterface.class);
 
-                                Log.d("userid" , SharePreferenceUtils.getInstance().getString("userid"));
-                                Log.d("pid" , pid);
-                                Log.d("quantity" , String.valueOf(stepperTouch.getCount()));
-                                Log.d("price" , nv1);
+                                Log.d("userid", SharePreferenceUtils.getInstance().getString("userid"));
+                                Log.d("pid", pid);
+                                Log.d("quantity", String.valueOf(stepperTouch.getCount()));
+                                Log.d("price", nv1);
 
                                 int versionCode = BuildConfig.VERSION_CODE;
                                 String versionName = BuildConfig.VERSION_NAME;
 
-                                Call<singleProductBean> call = cr.addCart(SharePreferenceUtils.getInstance().getString("userId") , pid , String.valueOf(stepperTouch.getCount()), nv1 , versionName);
+                                Call<singleProductBean> call = cr.addCart(SharePreferenceUtils.getInstance().getString("userId"), pid, String.valueOf(stepperTouch.getCount()), nv1, versionName);
 
                                 call.enqueue(new Callback<singleProductBean>() {
                                     @Override
                                     public void onResponse(Call<singleProductBean> call, Response<singleProductBean> response) {
 
-                                        if (response.body().getStatus().equals("1"))
-                                        {
+                                        if (response.body().getStatus().equals("1")) {
                                             //loadCart();
                                             dialog.dismiss();
                                         }
@@ -175,16 +186,13 @@ public class SingleProduct extends AppCompatActivity {
                             }
                         });
 
-                    }
-                    else
-                    {
+                    } else {
                         Toast.makeText(SingleProduct.this, "Please login to continue", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(SingleProduct.this , Login.class);
+                        Intent intent = new Intent(SingleProduct.this, Login.class);
                         startActivity(intent);
 
                     }
                 }
-
 
 
             }
@@ -222,23 +230,24 @@ public class SingleProduct extends AppCompatActivity {
             public void onResponse(Call<singleProductBean> call, Response<singleProductBean> response) {
 
 
-                if (response.body().getStatus().equals("1"))
-                {
+                if (response.body().getStatus().equals("1")) {
                     Data item = response.body().getData();
 
                     pid = item.getId();
 
-                    DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true).cacheOnDisk(true).resetViewBeforeLoading(false).build();
-                    ImageLoader loader = ImageLoader.getInstance();
-                    loader.displayImage(item.getImage() , image , options);
+                    List<String> urls = new ArrayList<>();
+                    urls.add(item.getImage());
+
+                    BannerAdapter adapter = new BannerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, urls);
+                    image.setAdapter(adapter);
+                    indicator.setViewPager(image);
 
                     float dis = Float.parseFloat(item.getDiscount());
 
-                    if (dis > 0)
-                    {
+                    if (dis > 0) {
 
                         float pri = Float.parseFloat(item.getPrice());
-                        float dv = (dis / 100 ) * pri;
+                        float dv = (dis / 100) * pri;
 
                         float nv = pri - dv;
 
@@ -246,21 +255,19 @@ public class SingleProduct extends AppCompatActivity {
 
                         discount.setVisibility(View.VISIBLE);
                         discount.setText(item.getDiscount() + "% OFF");
-                        price.setText(Html.fromHtml("<font color=\"#000000\"><b>\u20B9 " + String.valueOf(nv) + " </b></font><strike>\u20B9 " + item.getPrice() + "</strike>"));
-                    }
-                    else
-                    {
+                        price.setText(Html.fromHtml("Selling Price:  <font color=\"#000000\"><b>\u20B9" + String.valueOf(nv) + " </b></font><strike>\u20B9" + item.getPrice() + "</strike>"));
+                    } else {
 
                         nv1 = item.getPrice();
                         discount.setVisibility(View.GONE);
-                        price.setText(Html.fromHtml("<font color=\"#000000\"><b>\u20B9 " + String.valueOf(item.getPrice()) + " </b></font>"));
+                        price.setText(Html.fromHtml("Selling Price:  <font color=\"#000000\"><b>\u20B9" + String.valueOf(item.getPrice()) + " </b></font>"));
                     }
 
 
                     title.setText(item.getName());
 
                     brand.setText(item.getBrand());
-                    unit.setText(item.getUnit());
+                    unit.setText(item.getSize());
                     seller.setText(item.getSeller());
 
                     description.setText(item.getDescription());
@@ -270,18 +277,13 @@ public class SingleProduct extends AppCompatActivity {
                     disclaimer.setText(item.getDisclaimer());
 
 
-
-                    if (item.getStock().equals("In stock"))
-                    {
+                    if (item.getStock().equals("In stock")) {
                         add.setEnabled(true);
-                    }
-                    else
-                    {
+                    } else {
                         add.setEnabled(false);
                     }
 
                     stock.setText(item.getStock());
-
 
 
                 }
@@ -298,4 +300,60 @@ public class SingleProduct extends AppCompatActivity {
         });
 
     }
+
+    class BannerAdapter extends FragmentStatePagerAdapter {
+
+        List<String> blist = new ArrayList<>();
+
+        public BannerAdapter(@NonNull FragmentManager fm, int behavior, List<String> blist) {
+            super(fm, behavior);
+            this.blist = blist;
+        }
+
+        /*public BannerAdapter(FragmentManager fm, List<Banners> blist) {
+            super(fm);
+            this.blist = blist;
+        }*/
+
+        @Override
+        public Fragment getItem(int position) {
+            page frag = new page();
+            frag.setData(blist.get(position));
+            return frag;
+        }
+
+        @Override
+        public int getCount() {
+            return blist.size();
+            //return 1;
+        }
+    }
+
+
+    public static class page extends Fragment {
+
+        String url, tit, cid = "", image2;
+
+        ImageView image;
+
+        void setData(String url) {
+            this.url = url;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.banner_layout2, container, false);
+
+            image = view.findViewById(R.id.imageView3);
+
+            DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true).cacheOnDisk(true).resetViewBeforeLoading(false).build();
+            ImageLoader loader = ImageLoader.getInstance();
+            loader.displayImage(url, image, options);
+
+
+            return view;
+        }
+    }
+
 }
